57bd1dd1d5adc0f9edc8dd669dd7bbbedfc3e870
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-10 20:38:53 +0800

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Services from "./components/services";
 import Function from "./components/function";
 import Team from "./components/team";
 import Testimonials from "./components/testimonials";
+import Faq from "./components/faq";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Function/>
         <Team/>
         <Testimonials/>
+        <Faq/>
       </main>
     </div>
   );
