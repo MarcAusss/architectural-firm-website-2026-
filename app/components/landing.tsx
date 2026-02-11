bc6afa92f9596056fe7777157672c54f7f95eba4
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-12 06:59:58 +0800

    asdasdasd

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -1,7 +1,15 @@
+import { playfair } from "../fonts";
+
 export default function Landing() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+  return (
+    <div className="w-full h-screen bg-no-repeat bg-center bg-cover" style={{backgroundImage: "url('/images/mountain-meditation-bliss-stockcake.jpg')",}} >
+      <div className="">
+        <h1 className={`${playfair.className} text-5xl`}>
+          Healing Starts with a Single {" "}
+          <span className="italic">Conversation</span>
+        </h1>
+      </div>
+
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/sub-components/navbar";
 import Footer from "./components/sub-components/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { montserrat } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${montserrat.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >  
         <Navbar/>
         {children}
